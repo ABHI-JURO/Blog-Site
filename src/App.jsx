299ff1a7d8858034a2893bfcb8bcaa252cc0bde4
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-08-02 09:29:12 +0530

    created the base for the project added the env variables and also the backend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <h1 className='bg-gray-700 text-white h-screen text-center items-center'>BLOG SITE</h1>
+    </>
+  )
+}
+
+export default App
