2ea3410608db51d2b0770a7bb933ccb7f2f3b182
Abhishek Shrivastava <138434193+ABHI-JURO@users.noreply.github.com>
2026-08-21 12:18:22 +0530

    minor addition in RTE

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Editor } from "@tinymce/tinymce-react";
 
-export default function RTE() {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
